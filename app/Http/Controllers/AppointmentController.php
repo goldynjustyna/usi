872ee6cbde842c88d2d4cb8eb3e3b684ec346f58
Justyna Gołdyn <justyna.goldyn@onet.pl>
2017-05-09 23:31:00 +0200

    kontroler wizyt

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -7,5 +7,17 @@ use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
 {
-
+    public function delete_appointment($appointment_id) {    
+        Appointment::destroy($appointment_id);
+        return response()->json(['id' => $appointment_id]);
+    }
+    
+    public function read_appointment($appointment_id) {
+        $appointment = Appointment::find($appointment_id);
+        return response()->json($appointment);
+    }
+    
+    public function read_appointments() {
+        return response()->json(Appointment::all());
+    }
 }
