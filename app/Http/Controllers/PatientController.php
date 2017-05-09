2bb4e180a5bf8c9423744142a33c02b23d2b721a
Justyna Gołdyn <justyna.goldyn@onet.pl>
2017-05-09 23:25:11 +0200

    kontroler pacjenta

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -7,5 +7,23 @@ use Illuminate\Http\Request;
 
 class PatientController extends Controller 
 {
-
+    public function delete_patient($patient_id) {
+        Patient::destroy($patient_id);
+        return response()->json(['id' => $patient_id]);
+    }
+    
+    public function read_patient($patient_id) {
+        $patient = Patient::find($patient_id);
+        return response()->json($patient);
+    }
+    
+    public function read_patient_appointments($patient_id) {
+        return response()->json(Patient::find($patient_id)->appointments()->getResults());
+    }
+    
+    public function read_patient_appointment($patient_id, $appointment_id) {
+	$result = Appointment::all()->where('DOCTOR_id', intval($patient_id))->where('id', intval($appointment_id));
+	 
+        return response()->json($result->current());
+    }
 }
