769dc350a8ffe795f4cd3b76a42013b44c0bb824
Justyna Gołdyn <justyna.goldyn@onet.pl>
2017-05-09 23:09:35 +0200

    Kontrolery

---
+++ app/Http/Controllers/PatientController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Patient;
+use Illuminate\Http\Request;
+
+class PatientController extends Controller 
+{
+
+}
---
+++ app/Http/Controllers/SpecialityController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Speciality;
+use Illuminate\Http\Request;
+
+class SpecialityController extends Controller
+{
+
+}
