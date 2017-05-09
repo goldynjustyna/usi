eb2267fc0c5a3dc7b07b9b621c82e9708d51ea52
Justyna Gołdyn <justyna.goldyn@onet.pl>
2017-05-09 23:52:00 +0200

    tokeny csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,12 +6,15 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
 {
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array
-     */
     protected $except = [
-        //
+        'doctor/create',
+        'doctor/*/edit',
+        'doctor/*/delete', 
+        'doctor/*/appointment',
+	'patient/*/delete',
+	'appointment/*/delete',
+        'appointment/create',
+        'patient/create',
+	'patient/*/appointment'
     ];
 }
